e821e47c91082eb130c15f1e9f4491ffcd599080
Chau Tung Lam <nctl144@gmail.com>
2017-07-11 14:53:11 -0700

    connect the buttons in tree view controller

---
+++ Green/controllers/TreeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TreeViewController.swift
+//  Green
+//
+//  Created by Nguyễn Lâm on 7/11/17.
+//  Copyright © 2017 lam. All rights reserved.
+//
+
+import UIKit
+
+class TreeViewController: UIViewController {
+    @IBOutlet weak var cameraButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        print("camera button tapped")
+    }
+
+}
---
+++ Green/models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Green
+//
+//  Created by Nguyễn Lâm on 7/11/17.
+//  Copyright © 2017 lam. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
