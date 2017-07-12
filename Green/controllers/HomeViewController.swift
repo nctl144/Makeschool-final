acf1644e97ce46188e94ff6d6996a27f4cedef3e
Chau Tung Lam <nctl144@gmail.com>
2017-07-12 11:39:30 -0700

    implement customized button for facebook auth

--- Green/controllers/HomeViewController.swift
+++ Green/controllers/HomeViewController.swift
@@ -7,18 +7,78 @@
 //
 
 import UIKit
+import FacebookCore
 import FacebookLogin
 
 class HomeViewController: UIViewController {
-
+    let myLoginButton = UIButton(type: .custom)
+    let logOutButton = UIButton(type: .custom)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.center = view.center
         
-        view.addSubview(loginButton)
+        myLoginButton.backgroundColor = UIColor.darkGray
+        myLoginButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40);
+        myLoginButton.center = view.center;
+        myLoginButton.setTitle("Start Planting!", for: .normal)
+        
+        logOutButton.backgroundColor = UIColor.red
+        logOutButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40);
+        logOutButton.center = view.center;
+        logOutButton.setTitle("log out", for: .normal)
+        
+        myLoginButton.alpha = 0
+        logOutButton.alpha = 0
+        
+        logOutButton.addTarget(self, action: #selector(self.logOutButtonTapped), for: .touchUpInside)
+        myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
+        
+        // Handle clicks on the button
+        if let accessToken = AccessToken.current {
+            print("logged in")
+            print(accessToken)
+            // show the log out button if the user access token has existed
+            self.logOutButton.alpha = 1
+        } else {
+            print("logged out")
+            
+            // show the login button if the access token is not set
+            myLoginButton.alpha = 1
+        }
+        
+        // Add the button to the view
+        view.addSubview(myLoginButton)
+        view.addSubview(logOutButton)
+    }
+    
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                // hide the login button and show the logout button
+                self.logOutButton.alpha = 1
+                self.myLoginButton.alpha = 0
+                
+                print("Logged in!")
+                print(grantedPermissions)
+                print(accessToken)
+            }
+        }
+    }
+    
+    @objc func logOutButtonTapped() {
+        print("logging the user out")
+        let loginManager = LoginManager()
+        
+        // log the user out
+        loginManager.logOut()
+        self.logOutButton.alpha = 0
+        self.myLoginButton.alpha = 1
     }
 
     override func didReceiveMemoryWarning() {
