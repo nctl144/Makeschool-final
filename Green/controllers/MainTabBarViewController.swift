1e1c364ad34ae3ccade35138abf1d7eb5851929e
Chau Tung Lam <nctl144@gmail.com>
2017-07-13 15:01:00 -0700

    add the camera and photo library functionality

--- Green/controllers/MainTabBarViewController.swift
+++ Green/controllers/MainTabBarViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
-
+    let photoHelper = MGPhotoHelper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        delegate = self
 
         // Do any additional setup after loading the view.
         self.tabBar.barTintColor = UIColor(hexString: "26272b")
@@ -31,18 +34,18 @@ class MainTabBarViewController: UITabBarController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MainTabBarViewController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 3 {
+            // trigger the taking/choosing photo action
+            photoHelper.presentActionSheet(from: self)
+            return false
+        } else {
+            return true
+        }
     }
-    */
-
 }
 
 
