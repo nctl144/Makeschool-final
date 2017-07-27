2ec8eca63ae685f7658c8e81ca47bbf243eb8eb4
Chau Tung Lam <nctl144@gmail.com>
2017-07-27 15:12:03 -0700

    build the stages for the tree condition, delete the photo library tab for now

---
+++ Green/models/TreeStages.swift
@@ -0,0 +1,30 @@
+//
+//  TreeStages.swift
+//  Green
+//
+//  Created by Nguyễn Lâm on 7/27/17.
+//  Copyright © 2017 lam. All rights reserved.
+//
+
+import Foundation
+
+class TreeStages {
+    let stages: [String: [String : String]] = [
+        "stage1": [
+            "title": "Your plant is good",
+            "description": "You are doing great at taking care of the tree! Keep up the good work and water the tree everyday"
+        ],
+        "stage2": [
+            "title": "Your tree is growing!",
+            "description": "Your tree is growing fast. Keep up the good work"
+        ],
+        "stage3": [
+            "title": "Your tree needs water",
+            "description": "it has been a while since you last watered your tree. Your tree needs you to give it some water :)"
+        ],
+        "stage4": [
+            "title": "Your tree is at a critical condition",
+            "description": "Your tree needs water right now or it might become very weak"
+        ]
+    ]
+}
